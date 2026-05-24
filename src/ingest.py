18fb03c594b4b77c6ad25a773d7e66f716f48a6a
albertobrancogalp <alberto.f.branco@galp.com>
2026-05-24 16:17:40 +0100

    Changes to be committed: 	modified:   src/ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,35 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
 load_dotenv()
 
-PDF_PATH = os.getenv("PDF_PATH")
+# Verificação de carregamento do PDF e do processo de divisão
+root_dir = Path(__file__).parent.parent
+pdf_path = root_dir / os.getenv("PDF_NAME")
+print(f"Carregando PDF do caminho: {pdf_path}")
+document = PyPDFLoader(str(pdf_path)).load()
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).split_documents(document)
+if not splitter:
+    print("Nenhum documento foi gerado após a divisão. Verifique o processo de divisão.")
+else:
+    print(f"{len(splitter)} documentos foram gerados após a divisão.") 
+    
+# Verificação do processo de geração de embeddings
+embedding_model = OpenAIEmbeddings(model=os.getenv("OPENAI_EMBEDDING_MODEL"))
+embeddings = embedding_model.embed_documents([doc.page_content for doc in splitter])
+if not embeddings:
+    print("Nenhuma embedding foi gerada. Verifique o processo de geração de embeddings.")
+else:
+    print(f"{len(embeddings)} embeddings foram geradas.")
 
-def ingest_pdf():
-    pass
 
 
-if __name__ == "__main__":
-    ingest_pdf()
+
